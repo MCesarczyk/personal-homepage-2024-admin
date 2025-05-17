0df2053b1b8b72088cdc1f5b09cc2cfea9f8f4fb
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-05-17 15:20:22 +0200

    feat: implement login, add authorized dashboard and skills page

---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from '../features/auth/loginForm';
+
+export const LoginPage = () => {
+  return (
+    <div className="grid place-content-center h-full">
+      <LoginForm />
+    </div>
+  );
+};
---
+++ src/pages/skills.tsx
@@ -0,0 +1,7 @@
+export const SkillsPage = () => {
+  return (
+    <div className="m-20">
+      <h1 className="text-2xl mb-4">Skills page</h1>
+    </div>
+  );
+};
